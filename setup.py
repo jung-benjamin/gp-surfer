9c3c209ca0031d6c65b5a4d2826b08b955d114ce
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2021-07-06 10:39:30 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'gaussianprocesses',
+    version = '0.1.0',
+    packages = find_packages(include = ['gaussianprocesses', 'gaussianprocesses.*']),
+    description = 'A package for gaussian process regression with the theano backend.',
+    )
