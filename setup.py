dc452f562e47d36473a17e075ec9581ecc6b1e26
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2021-07-29 11:13:40 +0200

    Change the version in the setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'gaussianprocesses',
-    version = '0.1.0',
+    version = '0.1.0dev',
     packages = find_packages(include = ['gaussianprocesses', 'gaussianprocesses.*']),
     description = 'A package for gaussian process regression with the theano backend.',
     )
