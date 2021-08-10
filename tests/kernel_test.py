44823ee00293c2b02ed76bd14a028b295b499161
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2021-08-10 10:23:18 +0200

    Add tests to kernel_test
    The tests in kernel_test are changed to be compatible with
    the pytest package.

--- tests/kernel_test.py
+++ tests/kernel_test.py
@@ -5,17 +5,53 @@
 import numpy as np
 from gaussianprocesses import kernels
 
-p = [1., 1., 1., 1.]
-rng = np.random.default_rng()
-x_1 = rng.standard_normal((10,2))
-x_2 = rng.standard_normal((10,2))
+def create_kernel_input():
+    """Create test input for a kernel"""
+    p = [1., 1., 1., 1.]
+    rng = np.random.default_rng()
+    x_1 = rng.standard_normal((10,2))
+    x_2 = rng.standard_normal((10,2))
+    return p, x_1, x_2
 
-sqe = kernels.SquaredExponential(p)
-k, g = sqe(x_1, x_2, grad = True)
+def test_sqe_kernel_shape():
+    p, x1, x2 = create_kernel_input()
+    sqe = kernels.SquaredExponential(p)
+    k = sqe(x1, x2, grad = False)
+    assert k.shape == (10, 10)
 
-asqe = kernels.AnisotropicSquaredExponential(p)
-ak, ag = asqe(x_1, x_2, grad = True)
+def test_asqe_kernel_shape():
+    p, x1, x2 = create_kernel_input()
+    asqe = kernels.AnisotropicSquaredExponential(p)
+    k = asqe(x1, x2, grad = False)
+    assert k.shape == (10, 10)
 
-test = sqe + asqe
-tk, tg = test(x_1, x_2, grad = True)
-print(tk, tg)
+def test_sqe_asqe_kernel_sum():
+    p, x1, x2 = create_kernel_input()
+    sqe = kernels.SquaredExponential(p)
+    asqe = kernels.AnisotropicSquaredExponential(p)
+    sum_k = sqe + asqe
+    k = sum_k(x1, x2, grad = False)
+    assert k.shape == (10, 10)
+
+# sqe = kernels.SquaredExponential(p)
+# k, g = sqe(x_1, x_2, grad = True)
+# print('SQE kernel shape ', k.shape)
+# print('SQE gradient shape ', len(g))
+# for n in g:
+#     print(n.shape)
+# 
+# asqe = kernels.AnisotropicSquaredExponential(p)
+# ak, ag = asqe(x_1, x_2, grad = True)
+# print('ASQE kernel shape ', ak.shape)
+# print('ASQE gradient shape ', len(ag))
+# for n in ag:
+#     print(n.shape)
+# 
+# test = sqe + asqe
+# tk, tg = test(x_1, x_2, grad = True)
+# print('Sum kernel shape ', tk.shape)
+# print('Sum gradient shape ', len(tg))
+# for n in tg:
+#     print(n.shape)
+
+#print(tk, tg)
