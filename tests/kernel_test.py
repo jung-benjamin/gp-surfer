b6692cbc904453934662b6340abfb272bd3be55a
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2021-08-10 14:20:24 +0200

    Add tests for the linear kernel and modify other tests

--- tests/kernel_test.py
+++ tests/kernel_test.py
@@ -5,37 +5,47 @@
 import numpy as np
 from gaussianprocesses import kernels
 
-def create_kernel_input():
+def create_kernel_input(pars=4, shape=(10,2)):
     """Create test input for a kernel"""
-    p = [1., 1., 1., 1.]
+    p = list(np.ones(pars))
     rng = np.random.default_rng()
-    x_1 = rng.standard_normal((10,2))
-    x_2 = rng.standard_normal((10,2))
+    x_1 = rng.standard_normal(shape)
+    x_2 = rng.standard_normal(shape)
     return p, x_1, x_2
+
+def kernel_shape(kernel, x1, x2):
+    k = kernel(x1, x2, grad = False)
+    assert k.shape == (x1.shape[0], x2.shape[0])
+
+def kernel_gradient_length(kernel, x1, x2):
+    k, g = kernel(x1, x2, grad = True)
+    assert len(g) == len(kernel.parameters)
 
 def test_sqe_kernel_shape():
     p, x1, x2 = create_kernel_input()
-    sqe = kernels.SquaredExponential(p)
-    k = sqe(x1, x2, grad = False)
-    assert k.shape == (10, 10)
+    return kernel_shape(kernels.SquaredExponential(p), x1, x2)
 
 def test_asqe_kernel_shape():
     p, x1, x2 = create_kernel_input()
-    asqe = kernels.AnisotropicSquaredExponential(p)
-    k = asqe(x1, x2, grad = False)
-    assert k.shape == (10, 10)
+    return kernel_shape(kernels.AnisotropicSquaredExponential(p), x1, x2)
+
+def test_linear_kernel_shape():
+    p, x1, x2 = create_kernel_input()
+    return kernel_shape(kernels.Linear(p), x1, x2)
 
 def test_sqe_gradient_length():
-    p, x1, x2 = create_kernel_input()
-    sqe = kernels.SquaredExponential(p)
-    k, g = sqe(x1, x2, grad = True)
-    assert len(g) == len(sqe.parameters)
+    p, x1, x2 = create_kernel_input(pars = 3)
+    return kernel_gradient_length(kernels.SquaredExponential(p), x1, x2)
 
 def test_asqe_gradient_length():
     p, x1, x2 = create_kernel_input()
     asqe = kernels.AnisotropicSquaredExponential(p)
     k, g = asqe(x1, x2, grad = True)
     assert len(g) == len(asqe.parameters)
+
+def test_linear_gradient_length():
+    p, x1, x2 = create_kernel_input(pars = 5)
+    return kernel_gradient_length(kernels.Linear(p), x1, x2)
 
 def test_sqe_asqe_kernel_sum_shape():
     p, x1, x2 = create_kernel_input()
@@ -93,25 +103,3 @@ def test_sqe_asqe_gradient_product_length():
     k2, g2 = asqe(x1, x2, grad = True)
     assert len(g) == (len(g1) + len(g2))
 
-# sqe = kernels.SquaredExponential(p)
-# k, g = sqe(x_1, x_2, grad = True)
-# print('SQE kernel shape ', k.shape)
-# print('SQE gradient shape ', len(g))
-# for n in g:
-#     print(n.shape)
-# 
-# asqe = kernels.AnisotropicSquaredExponential(p)
-# ak, ag = asqe(x_1, x_2, grad = True)
-# print('ASQE kernel shape ', ak.shape)
-# print('ASQE gradient shape ', len(ag))
-# for n in ag:
-#     print(n.shape)
-# 
-# test = sqe + asqe
-# tk, tg = test(x_1, x_2, grad = True)
-# print('Sum kernel shape ', tk.shape)
-# print('Sum gradient shape ', len(tg))
-# for n in tg:
-#     print(n.shape)
-
-#print(tk, tg)
