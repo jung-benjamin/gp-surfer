f071ed5e8cc603d6d390a654446623cf425eed1a
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2021-08-10 16:05:44 +0200

    Edit version number
    Import the modules in the __init__.py to improve importability.

---
+++ gaussianprocesses/__init__.py
@@ -0,0 +1,3 @@
+import gaussianprocesses.kernels as kernels
+import gaussianprocesses.models as models
+import gaussianprocesses.processing as processing
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'gaussianprocesses',
-    version = '0.1.0dev',
+    version = '0.1.0',
     packages = find_packages(include = ['gaussianprocesses', 'gaussianprocesses.*']),
     description = 'A package for gaussian process regression with the theano backend.',
     )
