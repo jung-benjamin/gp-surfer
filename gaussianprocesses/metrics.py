5a9c8993fc36867d0a69f0736ab92f35d6cc72a6
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2021-08-27 15:48:33 +0200

    Add metrics module for gp model evaluation

---
+++ gaussianprocesses/metrics.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python3
+
+"""Metrics to evaluate quality of the GP models"""
+
+import numpy as np
+
+def total_sum_squares(data, axis=None):
+    '''Calculate the total sum of squares of errors
+
+    The squared sum of the difference between each data point
+    to the mean of the data is the total variance of the data
+    (not divided by the number of data points).
+
+    Parameters
+    ----------
+    data
+        (nx1) array, data points
+
+    Output
+    ------
+    ss_tot
+        float, total variance of array.
+
+    '''
+    if not isinstance(data, np.ndarray):
+        data = np.array(data)
+    mean = data.mean()
+    sq_diff = (data - mean)**2
+    ss_tot = sq_diff.sum()
+    return ss_tot
+
+def sum_squares_residuals(pred, data):
+    '''Calculate the sum of squares of the residual errors
+
+    The squared sum of the residuals (difference between model
+    prediction and data) represents the unexplained variance
+    of the model.
+
+    Parameters
+    ----------
+    pred 
+        (nx1) array, model predictions
+    data
+        (nx1) array, data points
+
+
+    Returns
+    -------
+    ss_res
+        float
+    '''
+    if not isinstance(data, np.ndarray):
+        data = np.array(data)
+    if not isinstance(pred, np.ndarray):
+        pred = np.array(pred)
+    res = (pred - data)**2 
+    ss_res = res.sum()
+    return ss_res
+    
+def r_squared(pred, data):
+    '''Calculate the R squared metric for predictions
+
+    R squared is the coefficient of determination of the 
+    model predictions. It is calculated by dividing the 
+    sum of squares of the residuals by the total sum of
+    squares and subtracting from 1.
+
+    Parameters
+    ----------
+    pred 
+        (nx1) array, model predictions
+    data
+        (nx1) array, data points
+
+    Returns
+    -------
+    Rsquare
+        float, R squared value
+    '''
+    ss_res = sum_squares_residuals(pred, data)
+    ss_tot = total_sum_squares(data)
+    r_square = 1 - (ss_res / ss_tot)
+    return r_square    
+
