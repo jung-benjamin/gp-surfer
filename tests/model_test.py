1992d5a4b47644a8af838e7b49da394d14f429a9
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2021-12-02 17:52:18 +0100

    Update tests for GPR model
    Not all functionalities of the class are included in the tests
    yet.

--- tests/model_test.py
+++ tests/model_test.py
@@ -6,6 +6,7 @@ import numpy as np
 from gaussianprocesses import models, kernels
 
 p = np.array([1., 1., 1., 1.])
+np.random.seed(10)
 rng = np.random.default_rng()
 x_1 = rng.standard_normal((10,2))
 y_1 = rng.standard_normal((10,1))
@@ -13,9 +14,9 @@ x_2 = rng.standard_normal((20,2))
 
 
 asqe = kernels.AnisotropicSquaredExponential(p)
-gpr = models.GaussianProcessRegression(x_data = x_1,
-                                       y_data = y_1,
-                                       kernel = asqe
+gpr = models.GaussianProcessRegression(x_train=x_1,
+                                       y_train=y_1,
+                                       kernel=asqe
                                        )
 
 gpr.optimize(n_steps = 10)
