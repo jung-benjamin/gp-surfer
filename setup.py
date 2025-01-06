1e5c30428d487f399909b0e3df41a1c0b4a6d29e
jung-benjamin <13368-jung-benjamin@users.noreply.git.rwth-aachen.de>
2025-01-06 15:35:38 +0100

    Update install procedure
    Add pyproject.toml file and move the install prescription from setup.py
    to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='gaussianprocesses',
-    version='0.5.1',
-    packages=find_packages(
-        include=['gaussianprocesses', 'gaussianprocesses.*']),
-    description='A package for gaussian process regression.',
-    entry_points={
-        'console_scripts': [
-            'train_gp_surrogates = gaussianprocesses.scripts:train_gp_surrogates',
-            'log_to_filepaths = gaussianprocesses.scripts:log_to_filepaths'
-        ]
-    },
-)
+setup()
